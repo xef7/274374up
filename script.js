b1c66b87411ebfc5f3561872108b713e8a091f2e
xef <45341061+xef7@users.noreply.github.com>
2024-02-28 08:36:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-function uploadLink() {
-  var linkInput = document.getElementById("linkInput");
-  var link = linkInput.value.trim();
-  
-  if (link === "") {
-    setStatusMessage("Please enter a valid link.", "error");
-    return;
-  }
+document.addEventListener('DOMContentLoaded', () => {
+    fetchUploadedLinksCount();
 
-  // إرسال الرابط إلى RequestBin هنا (يمكن استخدام AJAX أو Fetch)
+    const linkForm = document.getElementById('linkForm');
+    const statusMessage = document.getElementById('statusMessage');
 
-  setStatusMessage("Link uploaded successfully.", "success");
-  linkInput.value = "https://eocahn12rwzxr3j.m.pipedream.net";
-}
+    linkForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const linkInput = document.getElementById('linkInput');
+        const link = linkInput.value.trim();
+        if (link !== '') {
+            try {
+                const response = await fetch('/api/upload/link', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ link })
+                });
+                const data = await response.json();
+                if (response.ok) {
+                    setStatusMessage(data.message, 'success');
+                    linkInput.value = '';
+                    fetchUploadedLinksCount();
+                } else {
+                    setStatusMessage(data.error, 'error');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                setStatusMessage('An error occurred. Please try again later.', 'error');
+            }
+        } else {
+            setStatusMessage('Please enter a valid link.', 'error');
+        }
+    });
 
-function setStatusMessage(message, type) {
-  var statusMessage = document.getElementById("statusMessage");
-  statusMessage.textContent = message;
-  statusMessage.className = type;
-}
+    async function fetchUploadedLinksCount() {
+        try {
+            const response = await fetch('/api/links/count');
+            const data = await response.json();
+            if (response.ok) {
+                document.getElementById('count').textContent = data.count;
+            } else {
+                console.error('Error:', data.error);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function setStatusMessage(message, type) {
+        statusMessage.textContent = message;
+        statusMessage.className = type;
+    }
+});
